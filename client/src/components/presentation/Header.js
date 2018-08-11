4e38361973d10aaa86e9d07621d4b59977f19dfa
d-kang <david.kang714@gmail.com>
2018-08-11 11:47:22 -0700

    Modularize Header

---
+++ client/src/components/presentation/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import RIcon from 'react-native-vector-icons/FontAwesome5';
+import vars from '../../styles/vars'
+
+class Header extends Component {
+
+  render() {
+    return (
+      <View style={styles.navBar}>
+        <Text style={styles.header}>Picturegram</Text>
+
+        <View style={{ position: 'absolute', right: 20, }}>
+          <RIcon name="paper-plane" size={20} color={vars.$black1} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: 15,
+    height: 56,
+    backgroundColor: vars.$white2,
+    borderBottomColor: vars.$white3,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontFamily: "billabong",
+    fontSize: 40,
+    color: vars.$black1,
+  },
+
+});
+
+
+export default Header;
