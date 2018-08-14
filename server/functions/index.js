546df6bf132da9d4169cc2cba3e555d1a9cfd004
d-kang <david.kang714@gmail.com>
2018-08-13 20:12:05 -0700

    Update Resolver

--- server/functions/index.js
+++ server/functions/index.js
@@ -4,14 +4,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { makeExecutableSchema } = require('graphql-tools');
+const { databaseURL } = require('./secret/firebaseConfig');
 
-
-const app = express();
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.database();
-const ref = db.ref('poll');
+const ref = db.ref('graphql/hello');
 
+admin.database().ref().remove
+/* gql */
 const typeDefs = `
   type Query {
     hi: String!
@@ -19,11 +20,27 @@ const typeDefs = `
   }
 `;
 
+db.ref('users/').on('value', data => console.log('user added:', data.val()));
+// const findOne = _.ref('users').child('2')
+
+
 const resolvers = {
   Query: {
-    hi: () => 'hello!',
+    async hi(_, args, context, info) {
+      // _.ref('graphql/hello').remove()
+
+
+      // must somehow dynamically set key
+      // await _.ref('/users/2').set({id: 'h2'});
+
+      await _.ref('users/2').update({id: 'h4', somekey: 'foo'});
+
+
+
+      return 'helloooo!!!';
+    },
     hello(_, args, context, info) {
-      console.log({ _, args, context, info })
+      console.log({_})
       return args.msg;
     },
   },
@@ -32,14 +49,17 @@ const resolvers = {
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-
 });
 
+/* express */
+const app = express();
 app.use(bodyParser.json())
 
-
-
-app.use('/', graphqlHTTP({ schema, graphiql: true }));
+app.use('/', graphqlHTTP({
+  schema,
+  graphiql: true,
+  rootValue: db,
+}));
 
 exports.api = functions.https.onRequest(app);
 
